f4f65210c393cc7b51ac8058be34d353f52373c2
PAVELchicharacha <pavelkiprov@yandex.ru>
2024-10-09 23:49:05 +0200

    second

--- app/src/main/java/com/example/teaapp/MainActivity.kt
+++ app/src/main/java/com/example/teaapp/MainActivity.kt
@@ -1,10 +1,8 @@
 package com.example.teaapp
 
-import android.health.connect.datatypes.units.Volume
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -13,28 +11,19 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.teaapp.ui.theme.TeaAppTheme
-
-
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.*
 import androidx.compose.ui.text.input.PasswordVisualTransformation
-import androidx.navigation.NavHost
-import androidx.navigation.NavHostController
-import androidx.navigation.compose.rememberNavController
 
 
 class MainActivity : ComponentActivity() {
@@ -118,21 +107,23 @@ fun TeaList(teaItems: List<Tea>) {
         }
     }
 }
-
-
-
-@Preview(showBackground = true)
+//данные для чая
 @Composable
 fun PreviewTeaList() {
     val teas = listOf(
         Tea("Чай зеленый", "250 мл", "80°C"),
         Tea("Чай черный", "200 мл", "90°C"),
-        Tea("Чай белый", "300 мл", "85°C")
+        Tea("Чай белый", "300 мл", "80°C"),
+        Tea("Чай белый", "300 мл", "99°C"),
+        Tea("Чай афро", "300 мл", "14°C"),
+        Tea("Чай гойда", "300 мл", "1337°C"),
+        Tea("Чай белый", "300 мл", "81°C"),
+        Tea("Чай белый", "300 мл", "85°C"),
     )
     TeaList(teaItems = teas)
 }
 
-//все активности будут тут
+//все переходы между "экранами"
 @Composable
 fun MainScreen() {
     Column(
@@ -146,9 +137,9 @@ fun MainScreen() {
             Box(modifier = Modifier.weight(1f).padding(16.dp)){
                 when(route){
                     "AddTea"-> AddTea()
-                    "Favorite"-> TeaItem(tea = Tea("Чай зеленый", "250 мл", "80°C",))
-                    "Dinary"-> Dinary()
-                    "Profile"->Profile()
+                    "Favorite"-> PreviewTeaList()
+                    "Dinary"-> MyScreen1()
+                    "Profile"->MyScreen()
                 }
             }
             Row (modifier = Modifier.fillMaxWidth().padding(16.dp),
@@ -162,6 +153,8 @@ fun MainScreen() {
         }
     }
 }
+
+
 
 @Composable
 fun AddTea(){
@@ -184,6 +177,7 @@ fun TeaItem(tea: Tea) {
         )
     }
 }
+//афк тема,нужна только бля того,что бы раньше можно было перекючаться между "экранами"
 @Composable
 fun Dinary(){
     Text("Dinary")
@@ -191,4 +185,53 @@ fun Dinary(){
 @Composable
 fun Profile(){
     Text("profile")
+}
+
+
+
+
+
+
+
+//какая то тема для ввода данных  для laZzy column
+fun handleInput(input: String) {
+    // Обработка данных
+}
+
+@Composable
+fun MyScreen1() {
+    LazyColumn {
+        items(items = listOf("Item 1", "Item 2", "Item 3")) { item ->
+            Text(text = item)
+        }
+    }
+}
+
+@Composable
+fun MyScreen() {
+    var input by remember { mutableStateOf("") }
+    LazyColumn {
+        items(items = listOf("Item 1")) { item ->
+            TextField(value = input, onValueChange = { input = it })
+            Spacer(modifier = Modifier.height(16.dp))
+        }
+    }
+
+    var input1 by remember { mutableStateOf("") }
+    LazyColumn ( modifier = Modifier.padding(horizontal = 0.dp, vertical = 100.dp) ){
+        items(items = listOf("Item 1")) { item ->
+            TextField(value = input1, onValueChange = { input1 = it })
+            Spacer(modifier = Modifier.height(16.dp))
+        }
+    }
+
+    var input2 by remember { mutableStateOf("") }
+    LazyColumn ( modifier = Modifier.padding(horizontal = 0.dp, vertical = 200.dp) ){
+        items(items = listOf("Item 1")) { item ->
+            TextField(value = input2, onValueChange = { input2 = it })
+            Spacer(modifier = Modifier.height(16.dp))
+            Button(onClick = { handleInput(input) }) { Text("добавить") }
+
+        }
+    }
 }
