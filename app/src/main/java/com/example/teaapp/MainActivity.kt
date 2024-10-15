6078310b2ba2f192b7c324be4571d1552648b911
PAVELchicharacha <pavelkiprov@yandex.ru>
2024-10-15 19:09:10 +0200

    added fireBase auth and getting (custom)data

--- app/src/main/java/com/example/teaapp/MainActivity.kt
+++ app/src/main/java/com/example/teaapp/MainActivity.kt
@@ -6,6 +6,7 @@ import android.net.Uri
 import android.os.Build
 import android.os.Bundle
 import android.provider.MediaStore
+import android.renderscript.ScriptGroup.Input
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.rememberLauncherForActivityResult
@@ -17,46 +18,54 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.*
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.unit.dp
 import com.google.firebase.Firebase
-import com.google.firebase.auth.EmailAuthProvider
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.auth
 import com.google.firebase.firestore.firestore
 
-
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val auth = Firebase.auth
         setContent {
-            MyApp()
+            MyApp(auth)
         }
     }
 }
 
 @Composable
-fun MyApp() {
+fun MyApp(auth: FirebaseAuth) {
     var isRegistered by remember { mutableStateOf(false) }
     if (isRegistered) {
-        MainScreen()
+        MainScreen(auth)
     } else {
         RegistrationScreen(onRegister = { isRegistered = true })
     }
@@ -94,48 +103,27 @@ fun RegistrationScreen(onRegister: () -> Unit) {
             Text(text = "Sign In")
         }
         Spacer(modifier = Modifier.height(10.dp))
-        Button(onClick = {
+        Button(modifier = Modifier, onClick = {
             SignUp(auth, email.value, password.value)
             onRegister()
         }) {
             Text(text = "Sign Up")
         }
-        Spacer(modifier = Modifier.height(10.dp))
-//        Button(onClick = {
-//            SignOut(auth)
-//
-//        }) {
-//            Text(text = "Sign out")
-//        }
     }
 }
 
-public fun SignOut(auth: FirebaseAuth) {
+private fun SignOut(auth: FirebaseAuth) {
     auth.signOut()
+
 }
 
-public fun DeleteAccount(auth: FirebaseAuth, email: String, password: String) {
-    val cre = EmailAuthProvider.getCredential(email, password)
-    auth.currentUser?.reauthenticate(cre)?.addOnCompleteListener {
-        if (it.isSuccessful) {
-            auth.currentUser?.delete()?.addOnCompleteListener {
-                if (it.isSuccessful) {
-                    Log.d("myLog", "Account is Deleted")
-                } else {
-                    Log.d("myLog", "Account is NOT Deleted")
+private fun DeleteAccount(auth: FirebaseAuth) {
+    auth.currentUser?.delete()!!
+    Log.d("myLog", "delete is successfull")
 
-                }
-
-            }
-
-        } else {
-            Log.d("myLog", "Auth Exeption")
-
-        }
-    }
 }
 
-public fun SignIn(auth: FirebaseAuth, email: String, password: String) {
+private fun SignIn(auth: FirebaseAuth, email: String, password: String) {
     auth.signInWithEmailAndPassword(email, password).addOnCompleteListener {
         if (it.isSuccessful) {
             Log.d("myLog", "Sign up successfull")
@@ -145,7 +133,7 @@ public fun SignIn(auth: FirebaseAuth, email: String, password: String) {
     }
 }
 
-public fun SignUp(auth: FirebaseAuth, email: String, password: String) {
+private fun SignUp(auth: FirebaseAuth, email: String, password: String) {
     auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener {
         if (it.isSuccessful) {
             Log.d("myLog", "Sign up successfull")
@@ -188,7 +176,7 @@ fun PreviewTeaList() {
 
 //все переходы между "экранами"
 @Composable
-fun MainScreen() {
+fun MainScreen(auth: FirebaseAuth) {
     Column(
         modifier = Modifier
             .padding(16.dp)
@@ -205,8 +193,8 @@ fun MainScreen() {
                 when (route) {
                     "AddTea" -> lzcm()
                     "Favorite" -> PreviewTeaList()
-                    "Dinary" -> MyScreen1()
-                    "Profile" -> Profile()
+//                    "Dinary" ->
+                    "Profile" -> Profile(auth)
                 }
             }
             Row(
@@ -223,12 +211,6 @@ fun MainScreen() {
             }
         }
     }
-}
-
-
-@Composable
-fun AddTea() {
-    Text("Add Tea")
 }
 
 @Composable
@@ -249,15 +231,9 @@ fun TeaItem(tea: Tea) {
     }
 }
 
-//афк тема,нужна только для того,что бы раньше можно было перекючаться между "экранами"
-@Composable
-fun Dinary() {
-    Text("Dinary")
-}
-
 //профиль и выбор фото с телефона
 @Composable
-fun Profile() {
+fun Profile(auth: FirebaseAuth) {
 
     var imgUri by remember { mutableStateOf<Uri?>(null) }
     val context = LocalContext.current
@@ -273,11 +249,12 @@ fun Profile() {
     Button(onClick = { launcher.launch("image/*") }) {
         Text(text = "pick image")
     }
-//    Button(onClick = { DeleteAccount(auth, email, password) }) {
-//        Text(text = "delete account")
-//    }
-
-
+    Button(modifier = Modifier.padding(vertical = 50.dp), onClick = { DeleteAccount(auth) }) {
+        Text(text = "delete account")
+    }
+    Button(modifier = Modifier.padding(vertical = 100.dp), onClick = { SignOut(auth) }) {
+        Text(text = "SignOut account")
+    }
     Column(
         modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
@@ -294,69 +271,20 @@ fun Profile() {
             bitmap.value?.let { btm ->
                 Image(
                     bitmap = btm.asImageBitmap(),
-//                    imageVector = btm.asImageBitmap(),
                     contentDescription = null,
                     modifier = Modifier
-                        .size(400.dp)
+                        .size(300.dp)
                         .padding(20.dp)
-
+                        .clip(CircleShape)
                 )
             }
-
-        }
-
-    }
-
-}
-
-
-//какая то тема для ввода данных  для laZzy column
-fun handleInput(input: String) {
-    // Обработка данных
-    val fs = Firebase.firestore
-}
-
-@Composable
-fun MyScreen1() {
-    LazyColumn {
-        items(items = listOf("Item 1", "Item 2", "Item 3")) { item ->
-            Text(text = item)
-        }
-    }
-}
-
-///херь для добавления чего либо в lazy column
-@Composable
-fun MyScreen() {
-    var input by remember { mutableStateOf("") }
-    LazyColumn {
-        items(items = listOf("Item 1")) { item ->
-            TextField(value = input, onValueChange = { input = it })
-            Spacer(modifier = Modifier.height(16.dp))
-        }
-    }
-
-    var input1 by remember { mutableStateOf("") }
-    LazyColumn(modifier = Modifier.padding(horizontal = 0.dp, vertical = 100.dp)) {
-        items(items = listOf("Item 1")) { item ->
-            TextField(value = input1, onValueChange = { input1 = it })
-            Spacer(modifier = Modifier.height(16.dp))
-        }
-    }
-
-    var input2 by remember { mutableStateOf("") }
-    LazyColumn(modifier = Modifier.padding(horizontal = 0.dp, vertical = 200.dp)) {
-        items(items = listOf("Item 1")) { item ->
-            TextField(value = input2, onValueChange = { input2 = it })
-            Spacer(modifier = Modifier.height(16.dp))
-            Button(onClick = { handleInput(input) }) { Text("добавить") }
-
         }
     }
 }
 
 @Composable
 fun lzcm() {
+
     val fs = Firebase.firestore
     val list = remember {
         mutableStateOf(emptyList<Tea>())
@@ -370,7 +298,7 @@ fun lzcm() {
     ) {
         LazyColumn(
             modifier = Modifier
-                .fillMaxHeight(0.85f)
+                .fillMaxHeight(0.75f)
         ) {
             items(list.value) { tea ->
                 Card(
@@ -383,19 +311,41 @@ fun lzcm() {
                             .fillMaxWidth()
                             .wrapContentWidth()
                     )
-
                 }
             }
         }
         Spacer(modifier = Modifier.height(10.dp))
+        var name by remember { mutableStateOf("") }
+        var volume by remember { mutableStateOf("") }
+        var temperature by remember { mutableStateOf("") }
+
+        Row(
+            modifier = Modifier.padding(horizontal = 32.dp)
+        ) {
+            TextField( modifier = Modifier.width(80.dp),
+                value = name,
+                onValueChange = { name = it },
+                label = { Text("Name") }
+            )
+            TextField(modifier = Modifier.width(90.dp),
+                value = volume,
+                onValueChange = { volume = it },
+                label = { Text("Volume") }
+            )
+            TextField(modifier = Modifier.width(90.dp),
+                value = temperature,
+                onValueChange = { temperature = it },
+                label = { Text("Temp") }
+            )
+        }
         Button(modifier = Modifier
             .fillMaxWidth()
-            .padding(30.dp), onClick = {
+            .padding(16.dp), onClick = {
             fs.collection("Tea").document().set(
                 Tea(
-                    "tea",
-                    "300мл",
-                    "1337"
+                    name,
+                    volume,
+                    temperature
                 )
             )
         })
